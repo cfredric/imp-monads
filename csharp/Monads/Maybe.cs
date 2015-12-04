8b97d71a75008bc2b7c379b9822fb3be3c50a1f9
Ciaran Downey <yahehe4837@gmail.com>
2015-12-03 23:03:51 -0500

    Maybe

---
+++ csharp/Monads/Maybe.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Monads
+{
+    public interface Maybe<A> 
+    {
+        Maybe<B> FMap<B>(Func<A, B> f);
+        Maybe<B> Bind<B>(Func<A, Maybe<B>> f);
+    }
+
+    public class Nothing<A> : Maybe<A>
+    {
+        public Nothing() { }
+
+        public Maybe<B> FMap<B>(Func<A, B> f)
+        {
+            return new Nothing<B>();
+        }
+
+        public Maybe<B> Bind<B>(Func<A, Maybe<B>> f)
+        {
+            return new Nothing<B>();
+        }
+    }
+
+    public class Just<A> : Maybe<A>
+    {
+        private A a;
+
+        public Just(A a)
+        {
+            this.a = a;
+        }
+
+        public Maybe<B> FMap<B>(Func<A, B> f)
+        {
+            try
+            {
+                var result = f.Invoke(a);
+                return new Just<B>(result);
+            }
+            catch (Exception)
+            {
+                return new Nothing<B>();
+            }
+        }
+
+        public Maybe<B> Bind<B>(Func<A, Maybe<B>> f)
+        {
+            return f.Invoke(a);
+        }
+
+        public A Value { get { return a; } }
+    }
+}
