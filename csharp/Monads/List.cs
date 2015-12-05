fe8c07a2f117509d93543b49f891524c8e2c00fe
cdowney <cdowney@cimpress.com>
2015-12-04 22:09:13 -0500

    List implementation

---
+++ csharp/Monads/List.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Monads
+{
+    public class List<A>
+    {
+        private readonly IEnumerable<A> _values;
+
+        public List(IEnumerable<A> values)
+        {
+            this._values = values;
+        }
+
+        public List<B> FMap<B>(Func<A, B> f)
+        {
+            return new List<B>(_values.Select(f));
+        }
+
+        public List<B> Bind<B>(Func<A, List<B>> f)
+        {
+            return new List<B>(_values.Select(f).SelectMany(b => b.Values));
+        }
+
+        public IEnumerable<A> Values{ get { return _values; } } 
+    }
+}
