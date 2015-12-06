231a7799b79c0d459428c9367d2fe225f193b758
cfredric <chris.p.fredrickson@gmail.com>
2015-12-05 21:46:21 -0500

    Add tests for nondeterminism w/ List

--- csharp/Monads/UnitTests/ListTests.cs
+++ csharp/Monads/UnitTests/ListTests.cs
@@ -33,6 +33,38 @@ namespace Monads.UnitTests
 
         }
 
+		[Test]
+		public void List_Nondeterminism1()
+		{
+			var values = new[] { 1, 2, 3, 4, };
+			Func<int, List<int>> unAbs = x => new List<int>(new[] { x, -x });
+			var list = new List<int> (values);
+			var results = list.Bind<int> (unAbs);
+			var resultsValues = results.Values.ToArray();
+
+			foreach(var x in values)
+			{
+				Assert.Contains (x, resultsValues);
+				Assert.Contains (-x, resultsValues);
+			}
+		}
+
+		[Test]
+		public void List_Nondeterminism2()
+		{
+			var values = new[] { 1, 2, };
+
+			var list = new List<int> (values);
+			var results = list
+				.Bind<int> (x => new List<int>(new [] { x, -x, }))
+				.Bind<int> (x => new List<int>(new [] { x, x + 7, }));
+			var resultsValues = results.Values.ToArray();
+
+			foreach(var x in new [] { 1, 2, 8, 9, -1, -2, 6, 5, }){
+				Assert.Contains (x, resultsValues);
+			}
+		}
+
         private IEnumerable<int> Helper(int a)
         {
             while (a > 0)
