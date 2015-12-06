be055fcfa9ac2b0dd698b02ec2cb3bdd80995881
cfredric <chris.p.fredrickson@gmail.com>
2015-12-05 22:02:15 -0500

    remove IMonad, IFunctor since unused

--- csharp/Monads/IFunctor.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace Monads {
-
-	public interface IFunctor<F, A> 
-    {
-		F FMap<B>(Func<A, B> f);
-	}
-}
--- csharp/Monads/IMonad.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace Monads {
-
-	public interface IMonad<M, A> : IFunctor<M, A> 
-    {
-		M Bind(Func<A, M> f);
-	}
-}
