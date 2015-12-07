5dc2ad4e2446c6c0124f3a3fd7250d79755ad056
cfredric <chris.p.fredrickson@gmail.com>
2015-12-06 22:39:21 -0500

    fix indentation

--- csharp/Monads/State.cs
+++ csharp/Monads/State.cs
@@ -2,73 +2,78 @@ using System;
 
 namespace Monads
 {
-	public class State<S, A>
-	{
-		// This monad models a stateful computation, in a mutable environment. Not super useful in
-		// nonpure imperative languages, since everything by default is already a stateful 
-		// computation in a mutable environment, but it's still interesting. Good to know it exists.
-		private readonly Func<S, Tuple<A, S>> _runState;
+    public class State<S, A>
+    {
+        // This monad models a stateful computation, in a mutable environment
+        // (which is passed along to chained computations).  Not super useful
+        // in nonpure imperative languages, since everything by default is
+        // already a stateful computation in a mutable environment, but it's
+        // still interesting. Good to know it exists.
+        private readonly Func<S, Tuple<A, S>> _runState;
 
-		// This constructor acts as Haskell's "State" data constructor.
-		public State (Func<S, Tuple<A, S>> f)
-		{
-			this._runState = f;
-		}
+        // This constructor acts as Haskell's "State" data constructor.
+        public State (Func<S, Tuple<A, S>> f)
+        {
+            this._runState = f;
+        }
 
-		public static State<S, A> unit (A a)
-		{
-			// In Haskell:
-			// return x = State $ \s -> (a, s)
-			return new State<S, A> (s => new Tuple<A, S> (a, s));
-		}
+        public static State<S, A> unit (A a)
+        {
+            // In Haskell:
+            // return x = State $ \s -> (a, s)
+            return new State<S, A> (s => new Tuple<A, S> (a, s));
+        }
 
-		public State<S, B> Bind<B> (Func<A, State<S, B>> f)
-		{
-			// In Haskell:
-			// (State h) >>= f =
-			//      State $ \s ->
-			//          let (a, newState) = h s
-			//              (State g) = f a
-			//          in g newState
-			return new State<S, B> (
-				s => {
-						Tuple<A, S> tuple = this.Computation.Invoke(s);
-						State<S, B> g = f.Invoke(tuple.Item1);
-						return g.Computation.Invoke(tuple.Item2);
-					}
-				);
-		}
+        public State<S, B> Bind<B> (Func<A, State<S, B>> f)
+        {
+            // In Haskell:
+            // (State h) >>= f =
+            //      State $ \s ->
+            //          let (a, newState) = h s
+            //              (State g) = f a
+            //          in g newState
+            return new State<S, B> (
+                    s => {
+                    Tuple<A, S> tuple = this.Computation.Invoke(s);
+                    State<S, B> g = f.Invoke(tuple.Item1);
+                    return g.Computation.Invoke(tuple.Item2);
+                    }
+                    );
+        }
 
-		public State<S, B> Void<B> (State<S, B> s)
-		{
-			// This is the >> operator in Haskell: same thing as Bind, but it ignores its argument.
-			return this.Bind (x => s);
-		}
+        public State<S, B> Void<B> (State<S, B> s)
+        {
+            // This is the >> operator in Haskell: same thing as Bind,
+            // but it uses a function that ignores its argument.
+            return this.Bind (x => s);
+        }
 
-		public Func<S, Tuple<A, S>> Computation { get { return this._runState; } }
+        public Func<S, Tuple<A, S>> Computation { get { return this._runState; } }
 
-		// A stateful computation that just returns the current state.
-		public State<S, S> getState()
-		{
-			// In Haskell:
-			// get = \s -> (s, s)
-			return new State<S, S> (s => new Tuple<S, S> (s, s));
-		}
+        // A stateful computation that just returns the current state.
+        public State<S, S> getState()
+        {
+            // In Haskell:
+            // get = \s -> (s, s)
+            return new State<S, S> (s => new Tuple<S, S> (s, s));
+        }
 
-		// A stateful operation that replaces the current state with the given state.
-		public State<S, S> putState(S newState)
-		{
-			// In Haskell, this is implemented as:
-			// put newState = \s -> ((), newState)
-			// However, in C# apparently one can't make something similar to type Tuple <null, S>,
-			// so this also returns the state that it just set.
-			return new State<S, S> (s => new Tuple<S, S> (newState, newState));
-		}
+        // A stateful operation that replaces the current state with the given
+        // state.
+        public State<S, S> putState(S newState)
+        {
+            // In Haskell, this is implemented as:
+            // put newState = \s -> ((), newState)
+            // However, in C# apparently one can't make something similar to
+            // type Tuple <null, S>, so this also returns the state that it
+            // just set.
+            return new State<S, S> (s => new Tuple<S, S> (newState, newState));
+        }
 
-		public static Tuple<A, S> runState(State<S, A> computations, S initialState)
-		{
-			return computations._runState.Invoke (initialState);
-		}
-	}
+        public static Tuple<A, S> runState(State<S, A> computations, S initialState)
+        {
+            return computations.Computation.Invoke (initialState);
+        }
+    }
 }
 
--- csharp/Monads/UnitTests/StateTests.cs
+++ csharp/Monads/UnitTests/StateTests.cs
@@ -5,36 +5,36 @@ using NUnit.Framework;
 
 namespace Monads.UnitTests
 {
-	[TestFixture]
-	public class StateTests
-	{
-		// Set up some stateful computations to chain together.
+    [TestFixture]
+        public class StateTests
+        {
+            // Set up some stateful computations to chain together.
 
-		State<Stack<int>, int> pop =
-			new State<Stack<int>, int> (s => {
-				var x = s.Pop();
-				return new Tuple<int, Stack<int>> (x, s);
-			});
+            State<Stack<int>, int> pop =
+                new State<Stack<int>, int> (s => {
+                        var x = s.Pop();
+                        return new Tuple<int, Stack<int>> (x, s);
+                        });
 
-		Func<int,
-			State<Stack<int>, int>> push = x =>
-			new State<Stack<int>, int> ((Stack<int> s) => {
-				s.Push (x);
-				return new Tuple<int, Stack<int>> (0, s);
-			});
+            Func<int,
+                State<Stack<int>, int>> push = x =>
+                    new State<Stack<int>, int> ((Stack<int> s) => {
+                            s.Push (x);
+                            return new Tuple<int, Stack<int>> (0, s);
+                            });
 
-		[Test]
-		public void State_Stack()
-		{
-			State<Stack<int>, int> manipulations = push (3)
-				.Void (push(4))
-				.Void (push(5))
-				.Void (pop);
+            [Test]
+                public void State_Stack()
+                {
+                    State<Stack<int>, int> manipulations = push (3)
+                        .Void (push(4))
+                        .Void (push(5))
+                        .Void (pop);
 
-			Tuple<int, Stack<int>> result = State<Stack<int>, int>.runState(manipulations, new Stack<int> ());
-			Assert.AreEqual (5, result.Item1);
-			Assert.Contains (3, result.Item2);
-			Assert.Contains (4, result.Item2);
-		}
-	}
+                    Tuple<int, Stack<int>> result = State<Stack<int>, int>.runState(manipulations, new Stack<int> ());
+                    Assert.AreEqual (5, result.Item1);
+                    Assert.Contains (3, result.Item2);
+                    Assert.Contains (4, result.Item2);
+                }
+        }
 }
